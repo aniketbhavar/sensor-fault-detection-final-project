a66129bb21f29296438226b3095ad5261331881e
Aniket <aniketbhava0601@gmail.com>
2024-03-21 23:29:11 +0530

    training pipeline complete

--- sensor/exception.py
+++ sensor/exception.py
@@ -13,11 +13,11 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 
-class SensorException(Exception):
+class SensorException(Exception): 
     def __init__(self, error_message, error_detail:sys):
         """
         :param error_message: error message in string format
-        """
+        """ 
         super().__init__(error_message)
 
         self.error_message = error_message_detail(
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,22 +1,13 @@
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
-from sensor.entity.artifact_entity import ModelEvaluationArtifact,ModelPusherArtifact,ModelTrainerArtifact
-from sensor.entity.config_entity import ModelPusherConfig,ModelEvaluationConfig,ModelTrainerConfig
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact
+#from sensor.entity.artifact_entity import ModelEvaluationArtifact,ModelPusherArtifact,ModelTrainerArtifact
+#from sensor.entity.config_entity import ModelPusherConfig,ModelEvaluationConfig,ModelTrainerConfig
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
-from sensor.components.data_ingestion import DataIngestion
-from sensor.components.data_validation import DataValidation
-from sensor.components.data_transformation import DataTransformation
-from sensor.components.model_trainer import ModelTrainer
-from sensor.components.model_evaluation import ModelEvaluation
-from sensor.components.model_pusher import ModelPusher
-from sensor.cloud_storage.s3_syncer import S3Sync
-from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
-from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 
 class TrainPipeline:
-    is_pipeline_running=False
+    
     def __init__(self):
         training_pipeline_config = TrainingPipelineConfig()
         self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
@@ -25,11 +16,18 @@ class TrainPipeline:
 
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
-            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            #self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
             logging.info("Starting data ingestion")
-            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
-            return data_ingestion_artifact
+            #data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            #data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Data ingestion completed")
+            #return data_ingestion_artifact
         except  Exception as e:
             raise  SensorException(e,sys)
+        
+    
+    def run_pipeline(self):
+        try:
+            data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+        except Exception as e:
+            raise SensorException(e,sys)
