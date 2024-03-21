439366d2f50aeee23df16e45ed9ccdf273676bba
Aniket <aniketbhava0601@gmail.com>
2024-03-21 17:11:57 +0530

    todays task

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 from sensor.logger import logging
 import os,sys
-def test_exception():
-    try:
-        logging.info("we are dividing 1 by zero")
-        x=1/0
-    except Exception as e:
-        raise SensorException(e,sys)
-
-
+from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 
 
 
 if __name__ == '__main__':
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
-    #mongodb_client = MongoDBClient()
-    #print("collection name:", mongodb_client.database.list_collection_names())
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
